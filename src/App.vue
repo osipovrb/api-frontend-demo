974ecb9f637cd20ff2c08c35f0e08954a848583b
Uberpanda <osipov.rb@yandex.ru>
2023-03-19 23:10:18 +0800

    добавил анимацию

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,26 @@
 </script>
 
 <template>
-    <HeaderComponent />
-
-    <RouterView />
-
+    <header-component />
+    <router-view v-slot="{ Component }">
+        <transition name="fade" mode="out-in">
+            <component :is="Component" :key="$route.path" />
+        </transition>
+    </router-view>
     <notifications />
 </template>
 
 <style lang="scss">
     @import 'assets/theme.scss';
     @import 'vue-loading-overlay/dist/css/index.css';
+
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity 0.1s ease;
+    }
+
+    .fade-enter-from,
+    .fade-leave-to {
+        opacity: 0;
+    }
 </style>
